a9169172222e5dbbf162fcb9b7ad012589e81ce1
qq20967582 <47180347+qq20967582@users.noreply.github.com>
2020-03-30 12:45:36 +0800

    add class001.py

--- class001.py
+++ class001.py
@@ -1,4 +1,8 @@
-import matplotlib.pyplot as plt
 import numpy as np
 
-print("abc")
+stock_cnt = 200
+view_days = 504
+stock_day_change = np.random.standard_normal((200, 504))
+print(stock_day_change[-2:, -3:])
+print(stock_day_change[-2:, -3:].astype(int))
+print(np.around(stock_day_change[-2:, -5:], 2))
